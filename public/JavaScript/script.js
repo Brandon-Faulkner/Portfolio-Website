eb2c0697f1c27cd78a56de051ebe17919c37eb1c
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-03-23 16:51:14 -0500

    Added Desktop Nav Start

--- public/JavaScript/script.js
+++ public/JavaScript/script.js
@@ -23,7 +23,64 @@ if (mode == 'dark') {
     document.documentElement.classList.add("light");
 }*/
 
-// Navigation Menu
+//#region Header & Nav
+
+function Resize_Nav() {
+  var desktopNav = document.getElementById("desktop-nav");
+  var mobileNav = document.getElementById("mobile-nav");
+
+  if(window.innerWidth <= 800){
+    desktopNav.classList.remove("show-desktop");
+    desktopNav.classList.add("hide");
+
+    mobileNav.classList.remove("hide");
+    mobileNav.classList.add("show-mobile");
+    mobileNav.style.top = "-190px";
+  }
+  else {
+    desktopNav.classList.remove("hide");
+    desktopNav.classList.add("show-desktop");
+
+    mobileNav.classList.remove("show-mobile");
+    mobileNav.classList.add("hide");
+  } 
+}
+
+window.onload = Resize_Nav;
+window.onresize = Resize_Nav;
+
+// Header Auto Show and Hide
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var header = document.querySelector("header");
+  var desktopNav = document.getElementById("desktop-nav");
+  var mobileNav = document.getElementById("mobile-nav");
+
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0";
+    header.classList.add("is-active");
+
+    if(window.innerWidth <= 800){
+      mobileNav.style.top = "-190px";
+    }
+    else {
+      mobileNav.style.top = "-160px";
+    }   
+
+  } else {
+    header.style.top = "-150px";
+    header.classList.remove("is-active");
+
+    mobileNav.style.top = "-300px";
+  }
+
+  if(currentScrollPos == 0) {header.classList.remove("is-active");}
+  prevScrollpos = currentScrollPos;
+}
+
+// Mobile Navigation Menu
 toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
 toggle_open_text = 'Menu';
@@ -43,21 +100,4 @@ setTimeout(function(){
 	nav.classList.toggle('open');	
 }, 800);
 
-// Header Auto Show and Hide
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("header").style.top = "0";
-    if(window.innerWidth <= 800){
-      document.getElementById("mobile-nav").style.top = "-190px";
-    }
-    else {
-      document.getElementById("mobile-nav").style.top = "-160px";
-    }   
-  } else {
-    document.querySelector("header").style.top = "-150px";
-    document.getElementById("mobile-nav").style.top = "-300px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+//#endregion Header & Nav
